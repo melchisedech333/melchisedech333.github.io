031245a435cae2fbc5c9bf6f16025d3322656970
melchisedech333 <melchisedech333@gmail.com>
2022-03-19 00:23:37 -0300

    Homepage

---
+++ core/main.js
@@ -0,0 +1,14 @@
+
+/*
+** Script principal de controle das áreas do portfólio.
+**
+** Jesus, minha suave briga de puro amor ^_^
+*/
+
+$(document).ready(function(){
+
+    console.log('Verbum stared!')
+
+});
+
+
