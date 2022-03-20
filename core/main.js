e21b211089992c6b466996a355b21868ccc64c33
melchisedech333 <melchisedech333@gmail.com>
2022-03-19 22:39:11 -0300

    Updates

--- core/main.js
+++ core/main.js
@@ -9,6 +9,15 @@ $(document).ready(function(){
 
     console.log('Verbum stared!')
 
+    // Avança para div de conteúdo (usado na versão mobile para descer o scroll).
+    $('.btn-arrow-mobile').on('click', function(){
+        var offset = $(".site-area-content").offset();
+        $('html, body').animate({
+            scrollTop: offset.top,
+            scrollLeft: offset.left
+        }, 1000);
+    });
+
 });
 
 
