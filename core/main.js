7b89793582d74dacf582427d456780c7b81f61cd
melchisedech333 <melchisedech333@gmail.com>
2022-03-22 21:28:08 -0300

    Updates

--- core/main.js
+++ core/main.js
@@ -33,4 +33,94 @@ $(document).ready(function(){
     
 });
 
+/*
+** Controle do carregamento do JSON dos posts.
+*/
+
+var globalMaxPostsPerPage = 2;
+var globalCurrentPage = 1;
+var globalPosts = [];
+
+$(document).ready(function(){
+    var jsonPosts = '../'+ environment +'/posts.json?rand='+ Math.floor(Math.random() * 31337);
+
+    // Carrega posts do blog e lista artigos da primeira página.
+    $.get(jsonPosts, function(posts){
+        globalPosts = posts;
+
+        for (var a=0; a<posts.length; a++) {
+            if (a >= globalMaxPostsPerPage)
+                break;
+
+            var post = posts[a];
+            
+            var htmlPost = `
+                <a class="post-link post-item" href="`+ post.link +`" >
+                    <div class="row" >
+                        <div class="col-md-8" >
+                            <div class=" post-title" >
+                                `+ post.title +`
+                            </div>
+
+                            <div class="post-description" >
+                                `+ post.description +`
+                            </div>
+                        </div>
+
+                        <div class="col-md-4" >
+                            <div class="post-image" >
+                                <img src="`+ post.image +`" />
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            `;
+
+            $('.posts').append(htmlPost);
+        }
+    });
+
+    // Carrega mais posts.
+    $('.btn-load-posts').on('click', function(){
+        var postIndex = globalMaxPostsPerPage * globalCurrentPage;
+
+        for (var a=postIndex, b=0; a<globalPosts.length; a++, b++) {
+            if (b >= globalMaxPostsPerPage)
+                break;
+
+            var post = globalPosts[a];
+            
+            var htmlPost = `
+                <a class="post-link post-item" href="`+ post.link +`" >
+                    <div class="row" >
+                        <div class="col-md-8" >
+                            <div class=" post-title" >
+                                `+ post.title +`
+                            </div>
+
+                            <div class="post-description" >
+                                `+ post.description +`
+                            </div>
+                        </div>
+
+                        <div class="col-md-4" >
+                            <div class="post-image" >
+                                <img src="`+ post.image +`" />
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            `;
+
+            $('.posts').append(htmlPost);
+        }
+
+        globalCurrentPage++;
+
+        // Verifica se já chegou na última página.
+        if ((globalMaxPostsPerPage * globalCurrentPage) >= globalPosts.length)
+            $('.btn-load-posts').css('display', 'none');
+    });
+});
+
 
